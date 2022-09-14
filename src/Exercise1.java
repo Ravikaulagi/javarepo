38c59d054c0cc1e4160252230eaa902b7963b52c
RaviKaulagi <ravikoulagi1919india@gmail.com>
2022-09-14 11:09:50 +0530

    first exercise

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -10,23 +10,13 @@ public class Exercise1
         int tree_num=s.nextInt();
         int ret=0;
 
-        for(int i=1;i<=(num1_row*num2_col);i++)
-        {
-            int mid1=(num1_row*i);
-            int mid2=((num1_row * i) + 1);
-            if (tree_num <= num1_row || tree_num==mid1  || tree_num==mid2 )
-            {
-             ret=1;
-             break;
-            }
-        }
-        if(ret==1)
+      if(tree_num<num2_col||(tree_num%num2_col==0)||(tree_num%num2_col==1))
         {
             System.out.println("yes");
         }
         else
         {
-            System.out.println("false");
+            System.out.println("No");
         }
     }
 }
