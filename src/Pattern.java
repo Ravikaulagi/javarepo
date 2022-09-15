74367d3700b43ab2daaee2e65dba9640ecaf6737
RaviKaulagi <ravikoulagi1919india@gmail.com>
2022-09-15 10:04:10 +0530

    second exercise

--- src/Pattern.java
+++ src/Pattern.java
@@ -8,7 +8,7 @@ public class Pattern
         System.out.println("enter the length");
         Scanner s=new Scanner(System.in);
         int len=s.nextInt();
-        for(int i=1;i<=len;i++)
+        for(int i=1;i<=len&&i<=26;i++)
         {
             for(int j=1;j<=i;j++)
             {
